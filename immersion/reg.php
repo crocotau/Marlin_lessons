366c9b9ec9a245edc877e46959462ba62c45443b
crocotau <goshkout@gmail.com>
2021-04-12 18:34:32 +0300

    register - done

---
+++ immersion/reg.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$email = $_POST["email"];
+check_email($email);
+
+function check_email($email){
+    $pdo = new PDO("mysql:host=localhost;dbname=immersion", "im", "123123");
+    $sql = "SELECT * FROM users WHERE :email=email";
+    $state = $pdo->prepare($sql);
+    $state->execute(["email"=>$email]);
+    $email_check=$state->fetch(PDO::FETCH_ASSOC);
+    if (!empty($email_check)){
+        $_SESSION["danger"]="Почта ".$email." занята";
+        header("Location: page_register.php");
+        exit();
+    }
+    reg_user(email, password);
+}
+
+$email=$_POST["email"];
+$password=$_POST["password"];
+
+function reg_user($email, $password){
+
+    $pdo = new PDO("mysql:host=localhost;dbname=immersion", "im", "123123");
+    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $state = $pdo->prepare($sql);
+    $state->execute(["email"=>$email, "password"=>password_hash($password, PASSWORD_DEFAULT)]);
+    $_SESSION["success"]="Всё супер!";
+    header("Location: page_login.php");
+}
+
