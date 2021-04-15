3e2fe2eca7d5b4c2ac9dcf8df702c5461f1b8024
crocotau <goshkout@gmail.com>
2021-04-15 13:52:43 +0300

    authorisation - done

--- immersion/backend.php
+++ immersion/backend.php
@@ -6,24 +6,20 @@ $password = $_POST['password'];
 
 function check_login_pass($email, $password){
     $pdo = new PDO("mysql:host=localhost;dbname=immersion", "im", "123123");
-    $sql = "SELECT * FROM `users`";
+    $sql = "SELECT * FROM users";
     $sth = $pdo->prepare($sql);
     $sth->execute(['email'=>$email, 'password'=>$password]);
     $arr = $sth->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($arr as $item) {
-        if ($item['email']!=$email || $item['password']!=$password){
-            return false;
-        }else {
-            return true;
-        }
-
+    foreach($arr as $item) {
+        if ($item['email']==$email && $item['password']==$password){return true;}
     }
 }
 
 $chk = check_login_pass($email, $password);
 
-if ($chk==false){
-    $_SESSION['danger'] = "Не правильные email или пароль";
-} else{
+if ($chk){
     header("Location: users.php");
+} else{
+    $_SESSION['danger'] = "Не правильные email или пароль";
+    header("Location: login.php");
 }
