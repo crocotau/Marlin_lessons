b42e2e65db794d60ba4dd803ce70d37df1a80788
crocotau <goshkout@gmail.com>
2021-04-29 20:08:59 +0300

    edit user

---
+++ immersion/userEdit.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+require_once "functions.php";
+
+$username = $_POST['username'];
+$job = $_POST['job'];
+$phone = $_POST['phone'];
+$address = $_POST['address'];
+$logged_id = $_SESSION['id'];
+$edit_id = $_GET['id'];
+
+check_access_to_edit_user($logged_id, $edit_id);
+
+edit_user($username, $job, $phone, $address, $edit_id);
+
+message('success_edit', 'Профиль обновлён');
+
+redirect_to('users.php');
