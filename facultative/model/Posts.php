238f62d810d987c00def3d946fa2f1b068f1a7f2
crocotau <goshkout@gmail.com>
2021-05-07 10:42:43 +0300

    posts

--- facultative/model/Posts.php
+++ facultative/model/Posts.php
@@ -2,31 +2,34 @@
 class Posts
 {
     public $username, $post, $datePost, $id;
+    private $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = new PDO("mysql:host=localhost;dbname=immersion", "im", "123123");
+    }
 
     public function setPost($username, $post)
     {
         $this->datePost=date('H:i, d-m-Y');
-        $pdo = new PDO("mysql:host=localhost;dbname=immersion", "im", "123123");
-        $sql = "INSERT INTO `posts` (`id`, `username`, `post`, `date`) VALUES (NULL, :username, :post, '')";
-        $sth=$pdo->prepare($sql);
+        $sql = "INSERT INTO `posts` (`username`, `post`, `date`) VALUES (:username, :post, '')";
+        $sth=$this->pdo->prepare($sql);
         $sth->execute(['username'=>$username, 'post'=>$post]);
-        $this->id=$pdo->lastInsertId();
+        $this->id=$this->pdo->lastInsertId();
         $sql = "UPDATE `posts` SET `date` = '$this->datePost' WHERE `posts`.`id` = '$this->id'";
-        $sth=$pdo->prepare($sql);
+        $sth=$this->pdo->prepare($sql);
         $sth->execute(['date'=>$this->datePost]);
     }
 
     public function getPost(): array
     {
-        $pdo = new PDO("mysql:host=localhost;dbname=immersion", "im", "123123");
         $sql = "SELECT * FROM posts";
-        $sth=$pdo->prepare($sql);
+        $sth=$this->pdo->prepare($sql);
         $sth->execute();
         return $arr=$sth->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function delPost($id){
-        $pdo = new PDO("mysql:host=localhost;dbname=immersion", "im", "123123");
-        $pdo->prepare("DELETE FROM `posts` WHERE `posts`.`id` = $id")->execute();
+        $this->pdo->prepare("DELETE FROM `posts` WHERE `posts`.`id` = '$id'")->execute();
     }
 }
